e9cf62de279641b2006d39210eebffb10c3c6c83
Joshua Caponigro <joshcap@email.unc.edu>
2023-05-06 03:24:41 -0400

    better parameters, verbose mode

--- delete.c
+++ delete.c
@@ -5,7 +5,18 @@
 
 #define DEFAULT_PASSES 3
 
-int secure_delete(const char *filename, int passes) {
+void print_binary(const char *label, const char *data, long size)
+{
+    printf("%s:\n", label);
+    for (long i = 0; i < size; i++)
+    {
+        printf(" %02x", (unsigned char)data[i]);
+    }
+    printf("\n");
+}
+
+int secure_delete(const char *filename, int passes, int verbose)
+{
     FILE *file;
     long file_size;
     char *random_data;
@@ -14,7 +25,8 @@ int secure_delete(const char *filename, int passes) {
     srand((unsigned int)time(NULL));
 
     file = fopen(filename, "rb+");
-    if (!file) {
+    if (!file)
+    {
         perror("Error opening file");
         return 1;
     }
@@ -23,14 +35,32 @@ int secure_delete(const char *filename, int passes) {
     file_size = ftell(file);
 
     random_data = (char *)malloc(file_size);
-    if (!random_data) {
+
+    if (!random_data)
+    {
         perror("Error allocating memory");
         fclose(file);
         return 1;
     }
 
-    for (i = 0; i < passes; i++) {
-        for (j = 0; j < file_size; j++) {
+    if (verbose)
+    {
+        fseek(file, 0, SEEK_SET);
+        int read = fread(random_data, 1, file_size, file);
+        if (read != file_size)
+        {
+            perror("Error reading file");
+            free(random_data);
+            fclose(file);
+            return 1;
+        }
+        print_binary("Original data", random_data, file_size);
+    }
+
+    for (i = 0; i < passes; i++)
+    {
+        for (j = 0; j < file_size; j++)
+        {
             random_data[j] = rand() % 256;
         }
 
@@ -39,29 +69,84 @@ int secure_delete(const char *filename, int passes) {
         fflush(file);
     }
 
+    if (verbose)
+    {
+        print_binary("Random data", random_data, file_size);
+    }
+
     free(random_data);
     fclose(file);
     return remove(filename);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2 && argc != 3) {
-        printf("Usage: %s <file> [<passes>]\n", argv[0]);
+void print_usage(const char *prog_name)
+{
+    printf("Usage: %s [options] <file>\n", prog_name);
+    printf("Options:\n");
+    printf("  -v, --verbose         Show original and edited binary content\n");
+    printf("  -p NUM, --passes NUM  Set the number of overwrite passes (default: 3)\n");
+    printf("  -h, --help            Show this help message\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int verbose = 0;
+    int passes = DEFAULT_PASSES;
+    char *filename = NULL;
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0)
+        {
+            verbose = 1;
+        }
+        else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+        {
+            print_usage(argv[0]);
+            return 0;
+        }
+        else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--passes") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                passes = atoi(argv[++i]);
+                if (passes < 1)
+                {
+                    fprintf(stderr, "Invalid passes parameter: %s\n", argv[i]);
+                    return 1;
+                }
+                printf("Running with %d passes\n", passes);
+            }
+            else
+            {
+                fprintf(stderr, "Missing argument for %s\n", argv[i]);
+                return 1;
+            }
+        }
+        else if (filename == NULL)
+        {
+            filename = argv[i];
+        }
+        else
+        {
+            fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if (!filename)
+    {
+        print_usage(argv[0]);
         return 1;
     }
 
-    int passes = (argc == 3) ? atoi(argv[2]) : DEFAULT_PASSES;
-
-    if (passes < 1) {
-        fprintf(stderr, "Invalid passes parameter: %s\n", argv[2]);
-        return 1;
-    }
-
-    if (secure_delete(argv[1], passes) != 0) {
+    if (secure_delete(filename, passes, verbose) != 0)
+    {
         perror("Error securely deleting file");
         return 1;
     }
 
-    printf("File %s has been securely deleted.\n", argv[1]);
+    printf("File %s has been securely deleted.\n", filename);
     return 0;
 }
